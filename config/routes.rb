4047cc126de3ee0cd3c91770793bf78dd607dd3b
Madhava Hansen <Madhavah@gmail.com>
2017-05-27 17:44:57 -0400

    added like routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
+    resources :apartments, only: [:create, :show, :index] do
+      resources :quirks, only: [:create, :show, :index, :destroy]
+      resources :likes, only: [:create, :destroy, :index]
+    end
+  end
+
+  root 'static_pages#root'
 end
