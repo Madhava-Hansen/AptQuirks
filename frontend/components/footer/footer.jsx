079df8b690a1034effe11b49f8c1517ba20f88e6
Madhava Hansen <Madhavah@gmail.com>
2017-06-16 19:21:43 -0400

    updated image to cloud url

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -6,7 +6,10 @@ const Footer = () => {
       <div className="footer-content">
         <p className="footer-copyright">(c) 2017 Apartment Quirks LLC</p>
         <section className="footer-logo">
-          <img className="footer-pic" src="assets/quirks-logo.png" alt="logo"/>
+          <img
+            className="footer-pic"
+            src="http://res.cloudinary.com/aptquirks/image/upload/v1497653286/lrbahix2swlowj946cpi.png"
+            alt="logo"/>
           <h1 className="footer-logo-text">Apartment Quirks</h1>
         </section>
       </div>
