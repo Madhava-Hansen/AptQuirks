4fde5b936d510cdfdb4e7635452e7d6eaf838d10
Madhava Hansen <Madhavah@gmail.com>
2017-06-16 18:58:36 -0400

    moved logo to cloudinary

--- frontend/components/header/logo.jsx
+++ frontend/components/header/logo.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Logo = ({ redirectHome }) => (
-  <li>
-    <img src="assets/quirks-logo.png" alt="logo"/>
-    <h1><Link to="/home">Apartment Quirks</Link></h1>
-  </li>
-)
+const Logo = ({ redirectHome }) => {
+    return (
+      <li>
+        <img
+          src="http://res.cloudinary.com/aptquirks/image/upload/v1497653286/lrbahix2swlowj946cpi.png"
+          alt="logo"/>
+        <h1><Link to="/home">Apartment Quirks</Link></h1>
+      </li>
+    )
+}
 
 export default Logo;
