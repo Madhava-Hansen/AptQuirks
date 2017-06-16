da51c7db2fa9b9cff4dce98fe4e97c62ec020cf1
Madhava Hansen <Madhavah@gmail.com>
2017-06-15 21:00:38 -0400

    added lat and lon from apartment params object

--- frontend/components/apartment/apartment_search.jsx
+++ frontend/components/apartment/apartment_search.jsx
@@ -28,6 +28,8 @@ class ApartmentSearch extends React.Component {
     e.preventDefault();
     const address = this.autocomplete.getPlace();
     const fullAddress = address.formatted_address;
+    const lat = address.geometry.location.lat();
+    const lon = address.geometry.location.lng();
     const finalFullAddress = this.formatAddress(fullAddress);
     const apartmentParams = {apartment: {street_address: finalFullAddress, longitude: lon, latitude: lat}};
     this.props.createApartment(apartmentParams);
