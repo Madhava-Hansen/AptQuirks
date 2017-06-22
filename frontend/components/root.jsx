32ddcbf62e47a73f6f2e4073bf478b4c7813def4
Madhava Hansen <Madhavah@gmail.com>
2017-06-22 17:52:27 -0400

    added route for messages

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import App from './app';
 import { HashRouter as Router, Route } from 'react-router-dom';
-import ApartmentMap from './apartment_map/apartment_map';
-import ApartmentSearch from './apartment/apartment_search';
+import ConversationsIndexContainer from './conversation/conversation_index_container';
 import ApartmentShowContainer from './apartment/apartment_show_container';
 import SessionFormContainer from './session_form/session_form_container';
 import ProfileShowContainer from './profile/profile_show_container';
+import QuirkFormContainer from './quirks/quirk_form_container';
+import ApartmentMap from './apartment_map/apartment_map';
+import ApartmentSearch from './apartment/apartment_search';
 import Home from './greeting/home';
 import Footer from './footer/footer';
-import QuirkFormContainer from './quirks/quirk_form_container';
 import Header from './header/header';
+import App from './app';
 
 
 export const Root = ({ store }) => (
@@ -22,8 +23,9 @@ export const Root = ({ store }) => (
           <Route path="/login" component={ SessionFormContainer }></Route>
           <Route path="/signup" component={ SessionFormContainer }></Route>
           <Route path="/apartments/:id" component={ ApartmentShowContainer } ></Route>
-          <Route path="/profile" component={ProfileShowContainer}></Route>
-          <Route path="/addquirk/:id" component={QuirkFormContainer}></Route>
+          <Route path="/profile" component={ ProfileShowContainer }></Route>
+          <Route path="/addquirk/:id" component={ QuirkFormContainer }></Route>
+          <Route path="/messages/:id" component={ ConversationsIndexContainer } ></Route>
           <Route path="/" component={ Footer } ></Route>
         </div>
     </Router>
