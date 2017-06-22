b479df33a4d7c49d8b81425e90e6cb9188215d3e
Madhava Hansen <Madhavah@gmail.com>
2017-06-22 16:21:12 -0400

    updated logo to redirect home on li click

--- frontend/components/header/logo.jsx
+++ frontend/components/header/logo.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Logo = ({ redirectHome }) => {
+class Logo extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    debugger;
     return (
-      <li>
+      <li className="logo"  onClick={this.props.redirectHome}>
         <img
           src="http://res.cloudinary.com/aptquirks/image/upload/v1497653286/lrbahix2swlowj946cpi.png"
           alt="logo"/>
-        <h1><Link to="/home">Apartment Quirks</Link></h1>
+        <h1>Apartment Quirks</h1>
       </li>
     )
+  }
+
 }
 
 export default Logo;
