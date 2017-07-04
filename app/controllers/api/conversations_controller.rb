57a3ad89bcddbdab2913311e8e01b348379bcff0
Madhava Hansen <Madhavah@gmail.com>
2017-07-03 22:36:32 -0400

    tweaked class methods for fetching conversations

--- app/controllers/api/conversations_controller.rb
+++ app/controllers/api/conversations_controller.rb
@@ -1,8 +1,15 @@
 class Api::ConversationsController < ApplicationController
 
   def create
-    @conversation = Conversation.new(conversation_params)
-    if @conversation.save
+    if Conversation.with(params[:conversation][:receiver_id], params[:conversation][:sender_id]).present?
+      @conversation = Conversation.with(params[:conversation][:sender_id], params[:conversation][:receiver_id]).first
+    else
+    @conversation = Conversation.create!(conversation_params)
+    end
+    if @conversation
+      receiver_id = current_user.id == conversation_params[:receiver_id] ?
+       conversation_params[:sender_id] : conversation_params[:receiver_id]
+      @receiver = User.find(receiver_id)
       render 'api/conversations/show'
     else
       render json: @conversation.errors.full_messages
@@ -10,14 +17,30 @@ class Api::ConversationsController < ApplicationController
   end
 
   def index
+    @user = current_user
+    if @user
+      @conversations = Conversation.where("sender_id = ? OR receiver_id = ?", @user.id, @user.id)
+      render 'api/conversations/index'
+    else
+      render json: ["couldn't find conversations"], status: 404
+    end
   end
 
   def show
+    @conversation = Conversation.find(conversation_params[:id])
+    if @conversation
+      receiver_id = current_user.id == @conversation.receiver_id ?
+       @conversation.sender_id : @conversation.receiver_id
+      @receiver = User.find(receiver_id)
+      render 'api/conversations/show'
+    else
+      render json: @conversation.errors.full_messages
+    end
   end
 
   private
 
   def conversation_params
-    params.require(:conversation).permit(:user_one, :user_two)
+    params.require(:conversation).permit(:receiver_id, :sender_id, :receiver_username, :sender_username, :receiver_image_url, :id)
   end
 end
