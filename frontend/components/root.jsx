974fdcd761702f5391bc508f027ce2742b5e1544
Madhava Hansen <Madhavah@gmail.com>
2017-07-03 22:51:45 -0400

    added messagin routes

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -6,12 +6,16 @@ import ApartmentShowContainer from './apartment/apartment_show_container';
 import SessionFormContainer from './session_form/session_form_container';
 import ProfileShowContainer from './profile/profile_show_container';
 import QuirkFormContainer from './quirks/quirk_form_container';
+import QuirkIndexContainer from './quirks/quirk_index_container';
 import ApartmentMap from './apartment_map/apartment_map';
 import ApartmentSearch from './apartment/apartment_search';
 import Home from './greeting/home';
 import Footer from './footer/footer';
 import Header from './header/header';
+import MessageIndexContainer from './conversation/message_index_container';
+import NewMessageContainer from './conversation/new_message_container';
 import App from './app';
+
 
 
 export const Root = ({ store }) => (
@@ -25,7 +29,9 @@ export const Root = ({ store }) => (
           <Route path="/apartments/:id" component={ ApartmentShowContainer } ></Route>
           <Route path="/profile" component={ ProfileShowContainer }></Route>
           <Route path="/addquirk/:id" component={ QuirkFormContainer }></Route>
-          <Route path="/messages/:id" component={ ConversationsIndexContainer } ></Route>
+          <Route path="/inbox" component={ ConversationsIndexContainer } ></Route>
+          <Route path="/message/new" component={ NewMessageContainer }></Route>
+          <Route path="/messages/:id" component={ MessageIndexContainer }></Route>
           <Route path="/" component={ Footer } ></Route>
         </div>
     </Router>
