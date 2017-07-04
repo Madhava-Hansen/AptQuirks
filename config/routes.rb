bd942051435a7e5b2f3c49d04fb1ed65fdac4325
Madhava Hansen <Madhavah@gmail.com>
2017-07-03 22:41:24 -0400

    added routes for conversations and messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :update] do
-      resources :messages, only: [:create, :destroy, :index]
-      resources :conversations, only: [:create, :show, :index]
-    end
+    resources :users, only: [:create, :update, :index]
+    resources :messages, only: [:create, :destroy, :index, :show]
+    resources :conversations, only: [:create, :show, :index]
     resource :session, only: [:create, :destroy, :show]
     resources :apartments, only: [:create, :show, :index] do
       resources :quirks, only: [:create, :show, :index, :destroy]
       resources :likes, only: [:create, :destroy, :index]
+      resources :images, only: [:create, :index, :show]
     end
   end
 
