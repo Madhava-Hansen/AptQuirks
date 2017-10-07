eccea63a8d24a00a5f842ac0aa500af29ed20268
Madhava Hansen <Madhavah@gmail.com>
2017-10-07 15:52:00 -0400

    add links to footer nav

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
 
 const Footer = () => {
+
   return (
     <footer>
       <div className="footer-content">
         <ul className="footer-nav">
-          <li>messages</li>
-          <li>search</li>
-          <li>profile</li>
+          <li className="nav-link" > <Link to="/inbox">messages</Link></li>
+          <li className="nav-link" > <Link to="/home">search</Link> </li>
+          <li className="nav-link" > <Link to="/profile">profile</Link> </li>
         </ul>
         <section className="footer-logo">
           <img
@@ -16,7 +18,7 @@ const Footer = () => {
             alt="logo"/>
           <h1 className="footer-logo-text">Apartment Quirks</h1>
         </section>
-        <img className="social-icons" src="http://res.cloudinary.com/aptquirks/image/upload/v1506985694/apt_quirks_social_icons_zlnudw.png" atl="social-icons"></img>
+        <img className="social-icons" src="http://res.cloudinary.com/aptquirks/image/upload/v1506985694/apt_quirks_social_icons_zlnudw.png" alt="social-icons"></img>
         <p className="footer-copyright">
           (c) 2017 Apartment Quirks Inc.</p>
       </div>
@@ -24,4 +26,4 @@ const Footer = () => {
   )
 }
 
-export default Footer;
+export default withRouter(Footer);
