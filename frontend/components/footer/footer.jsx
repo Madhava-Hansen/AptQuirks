f32bf97d9789c813cf8a37d95dc555bedaaeb81a
Madhava Hansen <Madhavah@gmail.com>
2017-10-08 16:43:40 -0400

    add back to top div

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,14 +3,23 @@ import { withRouter, Link } from 'react-router-dom';
 
 const Footer = () => {
 
+  let backToTop = () => {
+    window.scrollTo(0, 0);
+  }
+
   return (
     <footer>
-      <div className="footer-content">
+      <div onClick={ backToTop } className="back-to-top">
+        <p className="back-to-top-text">back to top</p>
+      </div>
+      <div className="footer-nav-container">
         <ul className="footer-nav">
-          <li className="nav-link" > <Link to="/inbox">messages</Link></li>
-          <li className="nav-link" > <Link to="/home">search</Link> </li>
-          <li className="nav-link" > <Link to="/profile">profile</Link> </li>
+          <li className="nav-link-footer" > <Link to="/inbox">messages</Link></li>
+          <li className="nav-link-footer" > <Link to="/home">search</Link> </li>
+          <li className="nav-link-footer" > <Link to="/profile">profile</Link> </li>
         </ul>
+      </div>
+      <div className="footer-content">
         <section className="footer-logo">
           <img
             className="footer-pic"
@@ -18,7 +27,6 @@ const Footer = () => {
             alt="logo"/>
           <h1 className="footer-logo-text">Apartment Quirks</h1>
         </section>
-        <img className="social-icons" src="http://res.cloudinary.com/aptquirks/image/upload/v1506985694/apt_quirks_social_icons_zlnudw.png" alt="social-icons"></img>
         <p className="footer-copyright">
           (c) 2017 Apartment Quirks Inc.</p>
       </div>
