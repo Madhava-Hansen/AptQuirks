8f0f736bbf77cbe9f4e1af191196d575262c3014
Madhava Hansen <Madhavah@gmail.com>
2017-10-10 13:31:07 -0400

    pass current user to footer for nav toggle

---
+++ frontend/components/footer/footer_container.jsx
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import Footer from './footer';
+
+const mapStateToProps = ({ session }) => {
+  return {
+    currentUser: session.currentUser
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+});
+
+const FooterContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Footer);
+
+export default FooterContainer;
