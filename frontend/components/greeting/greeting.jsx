4e52a8d44cd38518250405cc111f29b9ee8c7c06
Madhava Hansen <Madhavah@gmail.com>
2018-03-17 15:01:53 -0400

    compile dropdown toggle logic to one document click event

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -10,40 +10,38 @@ class Greeting extends React.Component {
     this.routeToConversations = this.routeToConversations.bind(this);
     this.handleDropdownReveal = this.handleDropdownReveal.bind(this);
     this.handleMobileDropdown = this.handleMobileDropdown.bind(this);
+    this.toggleDropdown = this.toggleDropdown.bind(this);
     this.logInGuest = this.logInGuest.bind(this);
     this.setProfilePic = this.setProfilePic.bind(this);
-    this.hideNavDropdown = this.hideNavDropdown.bind(this);
     this.logout = this.logout.bind(this);
-    this.state = { dropdown: "hidden", navRight: "nav-right-reveal" };
+    this.state = { dropdown: "hidden", navRight: "nav-right" };
     this.picturePath = "https://res.cloudinary.com/aptquirks/image/upload/v1507410965/ksuq2q0estyfuw3y93rj.jpg"
     this.triggered = false;
   }
 
-  handleDropdownReveal(e) {
-    if (this.triggered === true) {
-      this.triggered = false;
+  handleDropdownReveal(event) {
+    if (event.target.id === "pic") {
+      this.toggleDropdown();
+    } else if (event.target.href) {
       return;
-    }
-    if (e === undefined && this.state.dropdown === "dropdown-class group") {
+    } else if (this.state.dropdown === "dropdown-class group") {
       this.setState({ dropdown: "hidden" });
-    } else if (this.state.dropdown === "hidden" && e !== undefined) {
-      this.setState({ dropdown: "dropdown-class group" });
-      this.triggered = true;
-    } else if (this.state.dropdown === "dropdown-class group" && e !== undefined) {
-      this.setState({ dropdown: "hidden" });
-      this.triggered = true;
     }
   }
 
   handleMobileDropdown() {
     if (this.state.navRight === "nav-right") {
-      this.setState({ navRight: "nav-right-reveal" }, () => {
-        console.log(this.state.navRight)
-      });
+      this.setState({ navRight: "nav-right-reveal" });
     } else if (this.state.navRight === "nav-right-reveal") {
-      this.setState({ navRight: "nav-right" }, () => {
-        console.log(this.state.navRight)
-      });
+      this.setState({ navRight: "nav-right" });
+    }
+  }
+
+  toggleDropdown() {
+    if (this.state.dropdown === "hidden") {
+      this.setState({ dropdown: "dropdown-class group" });
+    } else {
+      this.setState({ dropdown: "hidden" });
     }
   }
 
@@ -54,8 +52,8 @@ class Greeting extends React.Component {
   }
 
   componentDidMount() {
-    document.addEventListener("click", () => {
-      this.handleDropdownReveal();
+    document.addEventListener("click", (event) => {
+      this.handleDropdownReveal(event);
         });
   }
 
@@ -68,10 +66,6 @@ class Greeting extends React.Component {
     const { logout } = this.props;
     this.handleDropdownReveal();
     window.setTimeout(logout, 500);
-  }
-
-  hideNavDropdown() {
-    // this.setState({ navRight: "nav-right" });
   }
 
   setProfilePic() {
@@ -96,10 +90,9 @@ class Greeting extends React.Component {
           <ul className={this.state.navRight}>
             <li className="nav-link-pic">
               <figure
-                onClick={ this.handleDropdownReveal }
                 id="header-profile-pic"
                  className="header-profile-pic">
-                <img src={ this.picturePath } alt="profile pic"></img>
+                <img id="pic" src={ this.picturePath } alt="profile pic"></img>
               </figure>
               <div
                 className={ this.state.dropdown }>
