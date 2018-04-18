1a6afdf655d618616741285ecccf45824bb5e8ca
Madhava Hansen <Madhavah@gmail.com>
2018-04-18 00:55:15 -0400

    add images table

---
+++ db/migrate/20180418034110_create_images_table.rb
@@ -0,0 +1,12 @@
+class CreateImagesTable < ActiveRecord::Migration[5.0]
+  def change
+    create_table :images do |t|
+      t.string :url, null: false
+      t.string :thumbnail_url, null: false
+      t.integer :user_id, null: false
+      t.integer :apartment_id, null: false
+
+      t.timestamps
+    end
+  end
+end
