f2dba735f67c4cab7f493e470bfc78f03b876b09
Madhava Hansen <Madhavah@gmail.com>
2018-05-04 12:17:53 -0500

    add image slideshow section

--- frontend/components/images/image_index.jsx
+++ frontend/components/images/image_index.jsx
@@ -6,19 +6,29 @@ import { withRouter } from 'react-router-dom';
 class ImageIndex extends React.Component {
   constructor(props) {
     super(props);
+    this.slideshow = this.slideshow.bind(this);
+    this.state = { slideshowClass: "hidden" };
+  }
+
+  slideshow() {
+    if (this.state.slideshowClass ==="hidden") {
+      this.setState({ slideshowClass: "image-slideshow"});
+    } else {
+      this.setState({ slideshowClass: "hidden"});
+    }
+
   }
 
   componentWillMount() {
+    let idsObject = { image: {apartment_id: null} };
+    let aptId;
     if (this.props.apartmentId === undefined) {
-      const aptId = this.props.location.pathname.split('/').pop();
-      const idsObject = {image: {apartment_id: aptId }};
-      this.props.fetchImages(idsObject);
+      aptId = this.props.location.pathname.split('/').pop();
+      idsObject.image.apartment_id = aptId;
     } else {
-      this.props.fetchImages(this.props.apartmentShow.id);
+      idsObject.image.apartment_id = this.props.apartmentShow.id;
     }
-  }
-
-  componentWillReceiveProps(nextProps) {
+    this.props.fetchImages(idsObject);
   }
 
   render() {
@@ -39,15 +49,21 @@ class ImageIndex extends React.Component {
 
     return (
       <div className="image-index-container">
+        <div className="image-header-container">
         <AddImageButton
           addImage={ addImage }
           apartmentShow={ apartmentShow }
           currentUser={ currentUser }
          />
+           <div onClick={ this.slideshow } className="slideshow"> view slideshow</div>
+         </div>
         <section className="image-index">
           { images }
         </section>
-
+        <div className={ this.state.slideshowClass }>
+          <p onClick={ this.slideshow } className="close-image">x</p>
+          <img className="current-slideshow-image" src={ "https://res.cloudinary.com/aptquirks/image/upload/v1524247265/pjbxpk4klymgp9enjkia.jpg" } alt="img"></img>
+        </div>
       </div>
     )
 
