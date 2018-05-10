8cda3679dd557861fd4f633d65a8833ee756eca5
Madhava Hansen <Madhavah@gmail.com>
2018-05-10 16:47:43 -0400

    add image slideshow

--- frontend/components/images/image_index.jsx
+++ frontend/components/images/image_index.jsx
@@ -6,17 +6,78 @@ import { withRouter } from 'react-router-dom';
 class ImageIndex extends React.Component {
   constructor(props) {
     super(props);
-    this.slideshow = this.slideshow.bind(this);
-    this.state = { slideshowClass: "hidden" };
+    this.toggleSlideshow = this.toggleSlideshow.bind(this);
+    this.slideshowLeft = this.slideshowLeft.bind(this);
+    this.slideshowRight = this.slideshowRight.bind(this);
+    this.setImageURL = this.setImageURL.bind(this);
+    this.toggleScroll = this.toggleScroll.bind(this);
+    this.scrollStatus = "";
+    this.imageIdx = "";
+    this.state = { slideshowClass: "hidden", slideshowIndex: 0 };
   }
 
-  slideshow() {
-    if (this.state.slideshowClass ==="hidden") {
-      this.setState({ slideshowClass: "image-slideshow"});
+  toggleSlideshow(e) {
+    window.scroll(0, 0);
+    this.setImageURL(e);
+    this.toggleScroll();
+    if (this.state.slideshowClass === "hidden") {
+      this.setState({ slideshowClass: "image-slideshow" });
     } else {
-      this.setState({ slideshowClass: "hidden"});
+      this.setState({ slideshowClass: "hidden" });
+    }
+  }
+
+  setImageURL(e) {
+    if (e.target.id === "close-image") { return; };
+    let { imageIndex } = this.props;
+    if (e.target.classList.value === "slideshow") {
+      if (this.state.slideshowIndex !== 0) {
+        this.setState({ slideshowIndex: 0 });
+      }
+    } else {
+      imageIndex.forEach((image, idx) => {
+        if (image.url === e.target.src) {
+          this.setState({ slideshowIndex: idx });
+        }
+      });
+    }
+  }
+
+  toggleScroll() {
+    let body = $('body');
+    if (this.scrollStatus === "toggle-scroll") {
+      body.removeClass("toggle-scroll");
+      this.scrollStatus = "";
+    } else {
+      body.addClass("toggle-scroll");
+      this.scrollStatus = "toggle-scroll";
+    }
+  }
+
+  slideshowLeft() {
+    if (this.props.imageIndex.length <= 1) { return; };
+    let index = this.state.slideshowIndex;
+    let length = this.props.imageIndex.length - 1;
+    if (this.state.slideshowIndex > 0) {
+      index -= 1;
+    } else {
+      index++;
     }
 
+    this.setState({ slideshowIndex: index });
+  }
+
+  slideshowRight() {
+    if (this.props.imageIndex.length <= 1) { return; };
+    let index = this.state.slideshowIndex;
+    let length = this.props.imageIndex.length - 1;
+    if (this.state.slideshowIndex >= length) {
+      index = 0;
+    } else {
+      index++;
+    }
+
+    this.setState({ slideshowIndex: index });
   }
 
   componentWillMount() {
@@ -39,33 +100,45 @@ class ImageIndex extends React.Component {
         return (
           <ImageIndexItem
             image={ image }
+            toggleSlideshow={ this.toggleSlideshow }
             key={ idx }
+            id={ idx }
            />
         )
       })
     } else {
       images = [];
     }
+      if (!imageIndex) {
+        return (
+          <div></div>
+        )
+      } else {
+        return (
+          <div className="image-index-container">
+            <div className="image-header-container">
+            <AddImageButton
+              addImage={ addImage }
+              apartmentShow={ apartmentShow }
+              currentUser={ currentUser }
+             />
+               <div onClick={ this.toggleSlideshow } className="slideshow">view slideshow</div>
+             </div>
+            <section className="image-index">
+              { images }
+            </section>
+            <div className={ this.state.slideshowClass }>
+              <p onClick={ this.toggleSlideshow } id="close-image" className="close-image">x</p>
+              <div className="current-slideshow-image">
+                <img  src={ imageIndex[this.state.slideshowIndex].url } alt="img"></img>
+                <p onClick={ this.slideshowLeft } className="slideshow-left">left</p>
+                <p onClick={ this.slideshowRight } className="slideshow-right">right</p>
+              </div>
+            </div>
+          </div>
+        )
+      }
 
-    return (
-      <div className="image-index-container">
-        <div className="image-header-container">
-        <AddImageButton
-          addImage={ addImage }
-          apartmentShow={ apartmentShow }
-          currentUser={ currentUser }
-         />
-           <div onClick={ this.slideshow } className="slideshow"> view slideshow</div>
-         </div>
-        <section className="image-index">
-          { images }
-        </section>
-        <div className={ this.state.slideshowClass }>
-          <p onClick={ this.slideshow } className="close-image">x</p>
-          <img className="current-slideshow-image" src={ "https://res.cloudinary.com/aptquirks/image/upload/v1524247265/pjbxpk4klymgp9enjkia.jpg" } alt="img"></img>
-        </div>
-      </div>
-    )
 
   }
 }
