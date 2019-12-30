2bc57662d79e2c652b5de6001f5b3e896884d2fb
Madhava Hansen <madhavah@gmail.com>
2019-12-30 16:40:43 -0500

    use flex box for quirks index responsive styling

--- frontend/components/quirks/quirk_index_item.jsx
+++ frontend/components/quirks/quirk_index_item.jsx
@@ -10,26 +10,24 @@ const QuirkIndexItem = ({ that, quirk, deleteQuirk, currentUser }) => {
     quirkDeleteClass = "quirk-button"
   }
   return (
-    <aside className={classes}>
-      <li>
-        <section className="quirk-user-info">
-        <figure className="quirk-pic">
-          <img className="quirk-pic" src={quirk.user_pic}></img>
-        </figure>
-        <p className="quirk-username">{quirk.user_name}</p>
-        <p className="timestamp">{quirk.created_at} ago</p>
-        </section>
-        <section className="quirk-main-content">
-        <div className="group">
-        <h4 className="quirk-title">{quirk.title}</h4>
-        <p className="apartment-number">Apt {quirk.apt_number}</p>
-        </div>
-        <p className="quirk-body">{quirk.body}</p>
-        {/* <button className={quirkDeleteClass} onClick={() => deleteQuirk(quirkIds, that)}>Delete</button> */}
-        <div className="divider"></div>
-        </section>
-      </li>
-    </aside>
+    <li className={classes}>
+      <section className="quirk-user-info">
+      <figure className="quirk-pic">
+        <img className="quirk-pic" src={quirk.user_pic}></img>
+      </figure>
+      <p className="quirk-username">{quirk.user_name}</p>
+      <p className="timestamp">{quirk.created_at} ago</p>
+      </section>
+      <section className="quirk-main-content">
+      <div className="group">
+      <h4 className="quirk-title">{quirk.title}</h4>
+      <p className="apartment-number">Apt {quirk.apt_number}</p>
+      </div>
+      <p className="quirk-body">{quirk.body}</p>
+      {/* <button className={quirkDeleteClass} onClick={() => deleteQuirk(quirkIds, that)}>Delete</button> */}
+      <div className="divider"></div>
+      </section>
+    </li>
   )
 
 }
