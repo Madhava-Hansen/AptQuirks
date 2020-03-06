6577328b266205a0c12210a1db10995e6314801f
Madhava Hansen <madhavah@gmail.com>
2020-03-06 15:35:54 -0500

    sassify quirks index styling

--- frontend/components/quirks/quirk_index.jsx
+++ frontend/components/quirks/quirk_index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {QuirkIndexItem} from './quirk_index_item';
 import {withRouter} from 'react-router-dom';
-import {receiveQuirks} from '../../actions/quirk_actions';
 
 class QuirkIndex extends React.Component {
   constructor(props) {
@@ -30,31 +29,30 @@ class QuirkIndex extends React.Component {
 
   redirectToAddQuirk = () => {
     if (this.props.currentUser) {
-      this.props.history.push(`/addquirk/${this.props.apartmentId}`)
+      this.props.history.push(`/addquirk/${this.props.apartmentId}`);
     } else {
-      this.setState({addQuirkErrorClassName: "add-quirk-error-message"});
+      this.setState({addQuirkErrorClassName: "QuirksIndex-addQuirkErrorMessage"});
       window.setTimeout(() => {
         this.setState({addQuirkErrorClassName: 'hidden'});
       }, 3000)
-    }
+    };
   }
 
   render() {
     const {currentUser, apartmentShow} = this.props;
     return (
-      <aside className="quirks-index-container">
-        <div className="quirks-header group">
-          <button className="add-quirk-button" onClick={this.redirectToAddQuirk}>add quirk</button>
-          <p className="whats-a-quirk" onClick={this.handleRevealQuirkInfo}> - What's a quirk?</p>
+      <aside className="QuirksIndex">
+        <div className="QuirksIndex-headerSection">
+          <button className="QuirksIndex-addQuirkButton" onClick={this.redirectToAddQuirk}>add quirk</button>
+          <p className="QuirksIndex-whatsAQuirk" onClick={this.handleRevealQuirkInfo}> - What's a quirk?</p>
           <p 
-            id="add-quirk-error-message" 
             className={this.state.addQuirkErrorClassName}>
             Please login to add a quirk!
           </p>
-          <div className={this.state.revealQuirk ? 'reveal-quirk-info group' : 'hidden'}>
-            <div className="quirk-info-absolute">
-              <p onClick={this.handleRevealQuirkInfo} className="close-quirk-info">close</p>
-              <p className="whats-a-quirk-text">
+          <div className={this.state.revealQuirk ? 'QuirksIndex-revealQuirkInfo' : 'hidden'}>
+            <div className="QuirksIndex-quirkInfoAbsolute">
+              <p onClick={this.handleRevealQuirkInfo} className="QuirksIndex-closeQuirkInfo">close</p>
+              <p className="QuirksIndex-whatsAQuirkPopover">
                 A quirk is a story about what it was like living at a house or apartment. Help other people out
                  by telling your story about living at {apartmentShow.street_address}!
               </p>
