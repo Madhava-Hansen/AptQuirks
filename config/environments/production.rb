65200e76a74741fdb805999b99094c8ede7e5132
Madhava Hansen <madhavah@gmail.com>
2020-04-01 17:03:24 -0400

    use application specific password for smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
     domain: "gmail.com",
     enable_starttls_auto: true,
     user_name: 'apartmentquirks@gmail.com',
-    password: 'Ap@rtmentQuirks',
+    password: 'afxxhgfijmfqtnmk',
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
