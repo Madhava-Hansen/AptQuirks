486ee3528d21d2d62b0b7ff51db32adf9ee81b6c
Madhava Hansen <madhavah@gmail.com>
2020-04-01 13:57:10 -0400

    bypass images table migratino

--- db/migrate/20180418170504_create_images.rb
+++ db/migrate/20180418170504_create_images.rb
@@ -1,12 +1,12 @@
 class CreateImages < ActiveRecord::Migration[5.0]
-  def change
-    create_table :images do |t|
-      t.string :url, null: false
-      t.string :thumbnail_url, null: false
-      t.integer :user_id, null: false
-      t.integer :apartment_id, null: false
+  # def change
+  #   create_table :images do |t|
+  #     t.string :url, null: false
+  #     t.string :thumbnail_url, null: false
+  #     t.integer :user_id, null: false
+  #     t.integer :apartment_id, null: false
 
-      t.timestamps
-    end
-  end
+  #     t.timestamps
+  #   end
+  # end
 end
