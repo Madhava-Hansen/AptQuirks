1eead883b62f2131898608d5e9f363041d4e4c88
Madhava Hansen <madhavah@gmail.com>
2020-07-17 12:05:20 -0400

    add session util file

---
+++ frontend/util/session_util.js
@@ -0,0 +1,6 @@
+// helper functions used in more than one component
+
+export const validateEmail = email => {
+  const emailValidationRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailValidationRegex.test(email.toLowerCase());
+};
