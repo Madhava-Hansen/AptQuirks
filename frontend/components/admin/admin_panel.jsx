8364bccf9fbd0ee79819f999bf6b17a98751499d
Madhava Hansen <madhavah@gmail.com>
2020-07-19 00:40:36 -0400

    add error message for /admin

--- frontend/components/admin/admin_panel.jsx
+++ frontend/components/admin/admin_panel.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {fetchUsers} from '../../util/conversation_api_util';
+import {Link} from 'react-router-dom';
 
 export const AdminPanel = () => {
 
@@ -13,11 +14,20 @@ export const AdminPanel = () => {
 
   return (
     <div className="AdminPanel">
-      <h1 className="AdminPanel-pageTitle">Admin Panel</h1>
-      <div className="AdminPanel-usersData">
-        <h1 className="AdminPanel-usersDataTitle">Total Users</h1>
-        <h2 className="AdminPanel-usersDataTotalUsers">{totalUsers.length}</h2>
-      </div>
+      {(totalUsers && totalUsers.length > 0) ? (
+        <>
+          <h1 className="AdminPanel-pageTitle">Admin Panel</h1>
+          <div className="AdminPanel-usersData">
+            <h1 className="AdminPanel-usersDataTitle">Total Users</h1>
+            <h2 className="AdminPanel-usersDataTotalUsers">{totalUsers.length}</h2>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>You don't have permission to view this page.</div>
+          <Link style={{color: 'steelblue', marginTop: '12px'}} to="/home">Home</Link>
+        </>
+      )}
     </div>
   )
 }
