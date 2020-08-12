24bf7fc0328f6aa2d5bcea3f8af886d3b3b97611
Madhava Hansen <madhavah@gmail.com>
2020-08-12 15:21:23 -0400

    scroll to top of page when load fora about page

--- frontend/components/company/about_page.jsx
+++ frontend/components/company/about_page.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 export const AboutPage = () => {
+  
+  useEffect(() => {
+    scrollTo(0, 0);
+  }, [])
+
   return (
     <div className="AboutPage">
       <div className="AboutPage-mainContent">
--- frontend/components/header/logo.jsx
+++ frontend/components/header/logo.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBuilding} from '@fortawesome/fontawesome-free-solid';
 
 class Logo extends React.Component {
   constructor(props) {
@@ -11,10 +9,6 @@ class Logo extends React.Component {
     return (
       <li className="logo" onClick={this.props.redirectHome}>
         <div className="logo-icon">
-          {/* <FontAwesomeIcon 
-            size="2x"
-            icon={faBuilding}
-          /> */}
           <img src="https://res.cloudinary.com/aptquirks/image/upload/v1597259623/Screen_Shot_2020-08-11_at_3.57.32_PM_qqvtut.png"></img>
         </div>
         <h1 className="logo-text">Apartment Quirks</h1>
