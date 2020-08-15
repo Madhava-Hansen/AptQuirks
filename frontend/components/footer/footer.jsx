8214a33c922d32acb739f8743765378bc12f5e15
Madhava Hansen <madhavah@gmail.com>
2020-08-15 16:57:49 -0400

    update founded year in footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,30 +1,51 @@
 import React from "react";
-import {withRouter} from "react-router-dom";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {FooterNavLinks} from "./footer_nav_links";
+import { withRouter } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FooterNavLinks } from "./footer_nav_links";
 
-const Footer = ({currentUser}) => 
+const Footer = ({ currentUser }) => (
   <footer className="Footer">
     <FooterNavLinks isLoggedIn={currentUser} />
-    <p className="Footer-foundingDate">Founded 2016</p>
+    <p className="Footer-foundingDate">founded 2017</p>
     <div className="Footer-iconsWrapper">
-      <a className="Footer-socialIcon" target="_blank" href="https://www.facebook.com/apartmentquirks">
+      <a
+        className="Footer-socialIcon"
+        target="_blank"
+        href="https://www.facebook.com/apartmentquirks"
+      >
         <FontAwesomeIcon size="2x" icon={["fab", "facebook"]} />
       </a>
-      <a className="Footer-socialIcon" target="_blank" href="https://www.instagram.com/apartmentquirksco/">
+      <a
+        className="Footer-socialIcon"
+        target="_blank"
+        href="https://www.instagram.com/apartmentquirksco/"
+      >
         <FontAwesomeIcon size="2x" icon={["fab", "instagram"]} />
       </a>
-      <a className="Footer-socialIcon" target="_blank" href="https://twitter.com/apartmentquirks">
+      <a
+        className="Footer-socialIcon"
+        target="_blank"
+        href="https://twitter.com/apartmentquirks"
+      >
         <FontAwesomeIcon size="2x" icon={["fab", "twitter"]} />
       </a>
     </div>
     <div className="Footer-share">
-      <div className="fb-like" data-href="https://www.facebook.com/apartmentquirks/" data-width="300px" data-layout="button" data-action="like" data-size="small" data-share="true"></div>
+      <div
+        className="fb-like"
+        data-href="https://www.facebook.com/apartmentquirks/"
+        data-width="300px"
+        data-layout="button"
+        data-action="like"
+        data-size="small"
+        data-share="true"
+      ></div>
     </div>
     <p className="Footer-email">service@apartmentquirks.com</p>
     <p className="Footer-copyright">
       {`${String.fromCharCode(169)} 2020 Apartment Quirks Inc.`}
     </p>
   </footer>
+);
 
 export default withRouter(Footer);
