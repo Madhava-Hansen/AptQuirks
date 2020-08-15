4fa95bae454c11f95732cccfbfca689c80722f9d
Madhava Hansen <madhavah@gmail.com>
2020-08-15 16:45:05 -0400

    conditionally load walkscore

--- frontend/components/third_party_api_features/walkscore.jsx
+++ frontend/components/third_party_api_features/walkscore.jsx
@@ -1,50 +1,66 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-export const Walkscore = ({data}) => {
-
+export const Walkscore = ({ data }) => {
   const [walkscoreData, setWalkscoreData] = useState({});
 
   const getWalkscoreData = () => {
     return $.ajax({
-      method: 'GET',
-      url: '/third_party_apis',
-      data: data
-    })
-  }
+      method: "GET",
+      url: "/third_party_apis",
+      data: data,
+    });
+  };
 
   useEffect(() => {
-    {data && data.third_party_ap_is.address && (
-      getWalkscoreData(data).then(response => {
-        setWalkscoreData(response);
-      })
-    )}
-  }, [data])
+    {
+      data &&
+        data.third_party_ap_is.address &&
+        getWalkscoreData(data).then((response) => {
+          setWalkscoreData(response);
+        });
+    }
+  }, [data]);
 
   return (
-      <>
-        {walkscoreData && (
-          <div className="Walkscore">
-            <img className="Walkscore-logo" src={walkscoreData.logo_url}></img>
-            <div className="Walkscore-scoreWrapper">
-              <div className="Walkscore-badgeWrapper">
-                <img className="Walkscore-badge" src={`//pp.walk.sc/badge/walk/score/${walkscoreData.walkscore}.svg`}></img>
-                <h3 className="Walkscore-badgeDescription">{walkscoreData.description}</h3>
-              </div>
-              {walkscoreData.transit && (
-                <div className="Walkscore-badgeWrapper">
-                  <img className="Walkscore-badge" src={`//pp.walk.sc/badge/transit/score/${walkscoreData.transit.score}.svg`} />
-                  <p className="Walkscore-badgeDescription">{walkscoreData.transit.description}</p>
-                </div>
-              )}
-              {walkscoreData.bike && (
-                <div className="Walkscore-badgeWrapper">
-                  <img className="Walkscore-badge" src={`//pp.walk.sc/badge/bike/score/${walkscoreData.bike.score}.svg`} />
-                  <p className="Walkscore-badgeDescription">{walkscoreData.bike.description}</p>
-                </div>
-              )}
+    <>
+      {walkscoreData && walkscoreData.walkscore && (
+        <div className="Walkscore">
+          <img className="Walkscore-logo" src={walkscoreData.logo_url}></img>
+          <div className="Walkscore-scoreWrapper">
+            <div className="Walkscore-badgeWrapper">
+              <img
+                className="Walkscore-badge"
+                src={`//pp.walk.sc/badge/walk/score/${walkscoreData.walkscore}.svg`}
+              ></img>
+              <h3 className="Walkscore-badgeDescription">
+                {walkscoreData.description}
+              </h3>
             </div>
+            {walkscoreData.transit && (
+              <div className="Walkscore-badgeWrapper">
+                <img
+                  className="Walkscore-badge"
+                  src={`//pp.walk.sc/badge/transit/score/${walkscoreData.transit.score}.svg`}
+                />
+                <p className="Walkscore-badgeDescription">
+                  {walkscoreData.transit.description}
+                </p>
+              </div>
+            )}
+            {walkscoreData.bike && (
+              <div className="Walkscore-badgeWrapper">
+                <img
+                  className="Walkscore-badge"
+                  src={`//pp.walk.sc/badge/bike/score/${walkscoreData.bike.score}.svg`}
+                />
+                <p className="Walkscore-badgeDescription">
+                  {walkscoreData.bike.description}
+                </p>
+              </div>
+            )}
           </div>
-        )}
-      </>
-   )
-}
+        </div>
+      )}
+    </>
+  );
+};
