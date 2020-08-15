33d4011e69b19a2f776b27ef8c3c5dc7016030c8
Madhava Hansen <madhavah@gmail.com>
2020-08-15 16:22:29 -0400

    tweak styling and content for about apge

--- frontend/components/company/about_page.jsx
+++ frontend/components/company/about_page.jsx
@@ -1,30 +1,32 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 
 export const AboutPage = () => {
-  
   useEffect(() => {
     scrollTo(0, 0);
-  }, [])
+  }, []);
 
   return (
     <div className="AboutPage">
       <div className="AboutPage-mainContent">
-        <h1 className="AboutPage-headingText">The Original Apartment Review Site</h1>
-        <h3 className="AboutPage-subHeading">Founded 2016</h3>
+        <h1 className="AboutPage-headingText">
+          The Original Apartment Review Site
+        </h1>
+        <h3 className="AboutPage-subHeading">Founded 2017</h3>
         <p className="AboutPage-body">
-          Founded in 2016 by Madhava Hansen, Apartment Quirks is the original apartment review site. 
-          We empower our users with knowledge about apartments to help them make 
-          better choices when renting. After years of apartment hunting in NYC, we 
-          decided there had to be a better way. So, we created a place where 
-          renters could share their experiences about living in apartments. This 
-          insider information is the meat and potatoes of what makes Apartment Quirks 
-          so powerful for renters. It reduces the anxiety, the unknown, the worry that 
-          comes along with blindly hoping the apartment you rented doesn’t have a laundry 
-          list of hidden problems. Does the shower temperature change suddenly, are there 
-          bug or rodent problems in the building, are the neighbors noisy, is the super 
-          lazy? Don’t rent blindly, get to know your next apartment, before you sign a lease. 
+          Apartment Quirks is the original apartment review site. We empower our
+          users with knowledge about apartments to help them make better choices
+          when renting. After years of apartment hunting in NYC, we decided
+          there had to be a better way. So, we created a place where renters
+          could share their experiences about living in apartments. This insider
+          information is the meat and potatoes of what makes Apartment Quirks so
+          powerful for renters. It reduces the anxiety, the unknown, the worry
+          that comes along with blindly hoping the apartment you rented doesn’t
+          have a laundry list of hidden problems. Does the shower temperature
+          change suddenly, are there bug or rodent problems in the building, are
+          the neighbors noisy, is the super lazy? Don’t rent blindly, get to
+          know your next apartment, before you sign a lease.
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
